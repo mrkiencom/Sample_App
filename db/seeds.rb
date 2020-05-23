05be5609ee051bbca865641ade19c43204992b40
mrkiencom <kien3031999@gmail.com>
2020-05-23 10:21:15 +0700

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,10 @@
-# Users
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin:     true,
-             activated: true,
-             activated_at: Time.zone.now)
+User.creater name: "Admin",
+  email:"admin@mail.com",
+  password:"123123",
+  is_admin:true
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
+40.time do |i|
+  User.create name:Faker::Name.name,
+  email: "example-#{i+1}@railstutorial.org",
+  password:"123123"
 end
-
-# Microposts
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
-
-# Following relationships
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
