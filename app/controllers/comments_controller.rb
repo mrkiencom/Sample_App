fcea364dab8e8d7206ab225113a3718ed4b6bb8d
mrkiencom <kien3031999@gmail.com>
2020-05-23 08:25:35 +0700

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,39 @@
+class CommentsController < ApplicationController  
+  
+  def create
+  @micropost = Micropost.find(params[:micropost_id])
+  @comment = @micropost.comments.build(params[:comment].permit(:body))
+  @comment.user_id = current_user.id
+  @comment.save!
+  redirect_to micropost_path(@micropost)
+end  
+
+def destroy
+  @micropost = Micropost.find(params[:micropost_id])
+  @comment = @micropost.comments.find(params[:id])
+  @comment.destroy
+  redirect_to micropost_path(@micropost)
+end  
+
+def edit
+  @comment = Comment.find(params[:id])
+end  
+
+def update
+  @micropost = Micropost.find(params[:micropost_id])
+  @comment = @micropost.comments.find(params[:id])
+  if @comment.update(comment_params)
+    flash[:success] = "comment updated"
+    redirect_to micropost_path(@micropost)
+  else
+    render 'edit'
+  end
+end  
+
+private
+
+  def comment_params
+    params[:comment].permit(:body)
+  end
+
+end
